57f411823a0d867d7280cd38d4a95f09f6f6a365
Samuel Agyekum <agyekumsamuel055@gmail.com>
2024-10-20 10:12:42 +0000

    Create tables for book store

--- task_2.sql
+++ task_2.sql
@@ -26,7 +26,8 @@ CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY, -- Unique ID for each customer
     name VARCHAR(215) NOT NULL,                 -- Customer's name
     email VARCHAR(215) UNIQUE NOT NULL,         -- Customer's email (must be unique)
-    phone VARCHAR(15)                           -- Customer's phone number
+    phone VARCHAR(15)  
+    address TEXT                                  -- Customer's phone number(new column)
 );
 
 -- 6. Create the Orders table
